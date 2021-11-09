49d830240b76b247ba1b0804ecabe4eb52e2bc53
jnErfan <j.n.erfan420@gmail.com>
2021-11-10 02:02:20 +0600

    All Package Install

---
+++ src/Linux_Pc_Builder/Firebase/FirebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDzDXdC1rnPGep_EnoUfaIxvqSap9bbc_w",
+    authDomain: "linux-pc-builder.firebaseapp.com",
+    projectId: "linux-pc-builder",
+    storageBucket: "linux-pc-builder.appspot.com",
+    messagingSenderId: "933741516701",
+    appId: "1:933741516701:web:417796064cc056aaa98174"
+  };
