74e96ccf979a75b0561d1b06d2a260d2475636ae
jnErfan <j.n.erfan420@gmail.com>
2021-11-10 02:42:07 +0600

    React Router Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomeContainer from "./Linux_Pc_Builder/Pages/Home/HomeContainer/HomeContainer";
+import NavBaar from "./Linux_Pc_Builder/Pages/Shared/NavBar/NavBaar";
+import Footer from "./Linux_Pc_Builder/Pages/Shared/Footer/Footer";
+import OurBlogs from "./Linux_Pc_Builder/Pages/OurBlogs/OurBlogs";
+import Contract from "./Linux_Pc_Builder/Pages/Contract/Contract";
+import NotFound from "./Linux_Pc_Builder/Pages/NotFound/NotFound";
+import Login from "./Linux_Pc_Builder/LoginMethod/Login/Login";
+import SignUp from "./Linux_Pc_Builder/LoginMethod/SignUp/SignUp";
+import AdminLogin from "./Linux_Pc_Builder/LoginMethod/AdminLogin/AdminLogin";
+import ShippingDetails from "./Linux_Pc_Builder/Pages/ShippingDetails/ShippingDetails";
 
 function App() {
   return (
-    <div >
-      
+    <div>
+      <Router>
+        <NavBaar />
+        <Switch>
+          {/* Default Path  */}
+          <Route exact path="/">
+            <HomeContainer />
+          </Route>
+          {/* Home  */}
+          <Route path="/home">
+            <HomeContainer />
+          </Route>
+          {/* Shipping Details  */}
+          <Route path="/shippingDetails/:packageId">
+            <ShippingDetails />
+          </Route>
+          {/*  Our Blogs  */}
+          <Route path="/blogs">
+            <OurBlogs />
+          </Route>
+          {/* Contract Us  */}
+          <Route path="/contract">
+            <Contract />
+          </Route>
+          {/* Login  */}
+          <Route path="/login">
+            <Login />
+          </Route>
+          {/* Sign Up  */}
+          <Route path="/signUp">
+            <SignUp />
+          </Route>
+          {/* Admin Login  */}
+          <Route path="/signUp">
+            <AdminLogin />
+          </Route>
+          {/* Page Not Found  */}
+          <Route exact path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
