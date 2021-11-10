6e70e0c3c81b6a51b2afb359686796b2f93c5511
jnErfan <j.n.erfan420@gmail.com>
2021-11-10 18:22:41 +0600

    Header Clip Path  Complete

--- src/Linux_Pc_Builder/Pages/Home/Header/Banner/Banner.js
+++ src/Linux_Pc_Builder/Pages/Home/Header/Banner/Banner.js
@@ -5,43 +5,45 @@ import Typewriter from "typewriter-effect";
 
 const Banner = () => {
   return (
-    <div className="bannerContainer">
-      <Row sx={1} md={2} lg={2} className="bannerItem container pt-5">
-        <div className="col col-12 col-md-6 col-lg-6">
-          <h1
-            style={{
-              fontSize: "60px",
-              fontFamily: "'Rubik', sans-serif",
-              fontWeight: "bold",
-            }}
-            className=""
-          >
-            <span style={{ color: "orangered" }}> Linux </span>
-            <span style={{ color: "#171E56" }}> Desktop </span>
-            <span className="text-black">
-              <Typewriter
-                options={{
-                  strings: ["Builder", "House", "Online Shop"],
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
-            </span>
-          </h1>
-          <h3 className="mt-4 dreamPc">
-            Build Your Dream PC With Our Online Desktop Shop
-          </h3>
-          <Button
-            style={{ fontFamily: "'Concert One', cursive" }}
-            variant="outline-light py-3 px-3 mt-4"
-          >
-            See Our All Pc Collection
-          </Button>
-        </div>
-        <div className="col col-12 col-md-6 col-lg-6 image1">
-          {/* Sliding Desktop Image */}
-        </div>
-      </Row>
+    <div className="bannerContainer2">
+      <div className="bannerContainer">
+        <Row sx={1} md={2} lg={2} className="bannerItem container pt-5">
+          <div className="col col-12 col-md-6 col-lg-6">
+            <h1
+              style={{
+                fontSize: "60px",
+                fontFamily: "'Rubik', sans-serif",
+                fontWeight: "bold",
+              }}
+              className=""
+            >
+              <span style={{ color: "orangered" }}> Linux </span>
+              <span style={{ color: "#171E56" }}> Desktop </span>
+              <span className="text-black">
+                <Typewriter
+                  options={{
+                    strings: ["Builder", "House", "Online Shop"],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </span>
+            </h1>
+            <h3 className="mt-4 dreamPc">
+              Build Your Dream PC With Our Online Desktop Shop
+            </h3>
+            <Button
+              style={{ fontFamily: "'Concert One', cursive" }}
+              variant="outline-light py-3 px-3 mt-4"
+            >
+              See Our All Pc Collection
+            </Button>
+          </div>
+          <div className="col col-12 col-md-6 col-lg-6 image1">
+            {/* Sliding Desktop Image */}
+          </div>
+        </Row>
+      </div>
     </div>
   );
 };
