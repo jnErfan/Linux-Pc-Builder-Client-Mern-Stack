cb0666b1104a8cbf158817eb6565eb68f881e4bf
jnErfan <j.n.erfan420@gmail.com>
2021-11-12 05:10:35 +0600

    Create Admin Route

--- src/Linux_Pc_Builder/AdminRoute/AdminRoute.js
+++ src/Linux_Pc_Builder/AdminRoute/AdminRoute.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Spinner } from "react-bootstrap";
+import { Redirect, Route } from "react-router";
+import useAuth from "../Hooks/useAuth";
 
-const AdminRoute = () => {
+const AdminRoute = ({ children, ...rest }) => {
+  const { user, isLoading } = useAuth();
+  if (isLoading) {
     return (
-        <div>
-            
-        </div>
+      <div
+        className="text-center"
+        style={{ marginTop: "200px", marginBottom: "200px" }}
+      >
+        <Spinner
+          animation="border"
+          variant="secondary"
+          style={{ padding: "100px" }}
+        />
+      </div>
     );
+  }
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user?.email === "hypereyegaming@gmail.com" ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/adminLogin",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
 };
 
 export default AdminRoute;
