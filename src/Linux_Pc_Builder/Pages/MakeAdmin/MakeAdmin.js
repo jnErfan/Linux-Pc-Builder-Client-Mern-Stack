e62e8becdc5f6946b4ae3e490ac508fa0606d86a
jnErfan <j.n.erfan420@gmail.com>
2021-11-11 21:52:47 +0600

    Make Admin Complete

--- src/Linux_Pc_Builder/Pages/MakeAdmin/MakeAdmin.js
+++ src/Linux_Pc_Builder/Pages/MakeAdmin/MakeAdmin.js
@@ -1,93 +1,42 @@
-import React from "react";
-import { Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Form, FormControl } from "react-bootstrap";
 
 const MakeAdmin = () => {
+  const [checked, setChecked] = useState(false);
+  const checkedHandler = (params) => {
+    setChecked(params.target.checked);
+  };
+  console.log(checked);
   return (
-    <div>
-      <Row lg={2}>
-        <div className="col col-lg-6">
-          <h1>Make Admin</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-            inventore sint fuga! Quia cumque voluptatibus totam minus molestias
-            ipsa asperiores dolor nulla, dolores, recusandae voluptatum
-            provident doloribus maxime! Sint sit iste, molestiae, quis
-            voluptates sequi eos rem quibusdam quia tenetur similique facilis
-            veniam animi alias eveniet quod ipsa veritatis ratione maiores
-            repellendus optio. Eaque omnis laudantium quaerat sed, dolorum
-            cupiditate a blanditiis veritatis provident? Molestiae porro ullam
-            quam, nostrum neque ipsa enim doloremque amet blanditiis illo, aut
-            eum repellat deleniti ut, voluptate dignissimos quo cumque quia
-            numquam impedit itaque nisi officiis ipsam harum. Quo hic sequi
-            doloribus iure nemo animi maxime corrupti temporibus consectetur
-            explicabo ipsam necessitatibus nam, veniam qui eligendi autem
-            adipisci facilis molestias. Sapiente maiores nihil corrupti unde,
-            rem exercitationem sint tempora provident repellat accusantium
-            dolorum officia officiis ab est? Quisquam maxime tempora
-            voluptatibus doloremque adipisci itaque laborum autem officiis
-            eligendi corrupti laboriosam libero pariatur eius molestias cumque
-            illo, magni hic, cum, reiciendis odit voluptas? Quam laudantium
-            perspiciatis consequatur, libero sint laborum quia, esse deserunt
-            nobis modi eligendi, fugit possimus deleniti! Quod autem, rem atque
-            error cumque rerum aliquid earum dignissimos, distinctio et
-            inventore laboriosam corporis blanditiis, doloribus iure tempore
-            repudiandae officiis amet corrupti sed! Necessitatibus facere vitae,
-            ad distinctio, culpa ut, quod adipisci animi omnis tempore itaque
-            laudantium. Id tenetur vitae omnis impedit beatae voluptate placeat
-            eius ipsam aut earum sequi aperiam sint odio et corrupti, mollitia
-            cupiditate cum? Dolores obcaecati numquam cupiditate dicta. Ipsa
-            aperiam autem, cupiditate illo deleniti nulla fugiat quisquam,
-            similique iure aspernatur aliquid? Quis, nulla alias deleniti,
-            architecto tempora recusandae laudantium laborum rem asperiores est
-            quia officia. Adipisci odit alias at in est aspernatur quidem
-            debitis id culpa nam quo harum, tempore recusandae, illo quae
-            repellendus beatae provident. Praesentium nemo minima nobis harum
-            fuga nostrum placeat, omnis dicta, dolore, tempora labore quidem
-            officiis?
-          </p>
+    <div style={{ marginTop: "50px", marginBottom: "100px" }}>
+      <h1 className="text-center ms-5" style={{ color: "#2e2e66" }}>
+        Make <span style={{ color: "orangered" }}>ADMIN</span>
+      </h1>
+      <form>
+        <div className="shadow-lg p-5 rounded mt-4">
+          <FormControl
+            className="my-3 py-3"
+            type="email"
+            placeholder="Email"
+            required
+          />
+          <Form.Group className="mb-3" controlId="formHorizontalCheck">
+            <Form.Check onChange={checkedHandler} label="Remove Admin ?" />
+          </Form.Group>
+          {!checked ? (
+            <button
+              className="btn w-100 text-white py-3 mt-4"
+              style={{ backgroundColor: "#2e2e66" }}
+            >
+              Promote To Admin
+            </button>
+          ) : (
+            <button className="btn btn-danger w-100 text-white py-3 mt-4">
+              Demote To Admin
+            </button>
+          )}
         </div>
-        <div className="col col-lg-6">
-          <h1>Make Admin</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil
-            inventore sint fuga! Quia cumque voluptatibus totam minus molestias
-            ipsa asperiores dolor nulla, dolores, recusandae voluptatum
-            provident doloribus maxime! Sint sit iste, molestiae, quis
-            voluptates sequi eos rem quibusdam quia tenetur similique facilis
-            veniam animi alias eveniet quod ipsa veritatis ratione maiores
-            repellendus optio. Eaque omnis laudantium quaerat sed, dolorum
-            cupiditate a blanditiis veritatis provident? Molestiae porro ullam
-            quam, nostrum neque ipsa enim doloremque amet blanditiis illo, aut
-            eum repellat deleniti ut, voluptate dignissimos quo cumque quia
-            numquam impedit itaque nisi officiis ipsam harum. Quo hic sequi
-            doloribus iure nemo animi maxime corrupti temporibus consectetur
-            explicabo ipsam necessitatibus nam, veniam qui eligendi autem
-            adipisci facilis molestias. Sapiente maiores nihil corrupti unde,
-            rem exercitationem sint tempora provident repellat accusantium
-            dolorum officia officiis ab est? Quisquam maxime tempora
-            voluptatibus doloremque adipisci itaque laborum autem officiis
-            eligendi corrupti laboriosam libero pariatur eius molestias cumque
-            illo, magni hic, cum, reiciendis odit voluptas? Quam laudantium
-            perspiciatis consequatur, libero sint laborum quia, esse deserunt
-            nobis modi eligendi, fugit possimus deleniti! Quod autem, rem atque
-            error cumque rerum aliquid earum dignissimos, distinctio et
-            inventore laboriosam corporis blanditiis, doloribus iure tempore
-            repudiandae officiis amet corrupti sed! Necessitatibus facere vitae,
-            ad distinctio, culpa ut, quod adipisci animi omnis tempore itaque
-            laudantium. Id tenetur vitae omnis impedit beatae voluptate placeat
-            eius ipsam aut earum sequi aperiam sint odio et corrupti, mollitia
-            cupiditate cum? Dolores obcaecati numquam cupiditate dicta. Ipsa
-            aperiam autem, cupiditate illo deleniti nulla fugiat quisquam,
-            similique iure aspernatur aliquid? Quis, nulla alias deleniti,
-            architecto tempora recusandae laudantium laborum rem asperiores est
-            quia officia. Adipisci odit alias at in est aspernatur quidem
-            debitis id culpa nam quo harum, tempore recusandae, illo quae
-            repellendus beatae provident. Praesentium nemo minima nobis harum
-            fuga nostrum placeat, omnis dicta, dolore, tempora labore quidem
-            officiis?
-          </p>
-        </div>
-      </Row>
+      </form>
     </div>
   );
 };
